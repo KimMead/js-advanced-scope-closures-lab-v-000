13f614bb5fc7f63acfc070bd0e8634f42f2bd0ab
Kimberly Ann Meadows <kimmeadows8@gmail.com>
2019-06-24 04:12:16 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,17 @@
+function produceDrivingRange(blockRange) {
+  return function (block1, block2) {
+    let range = parseInt(block2) - parseInt(block1)
+
+    if (range < blockRange) {
+      return `within range by ${blockRange - range}`
+    } else {
+      return `${range - blockRange} blocks out of range`
+    }
+  }
+}
+
+function produceTipCalculator(tipPercent) {
+  return function(total) {
+    return total * tipPercent 
+  }
+}
